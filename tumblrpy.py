7edffe91b4996b772460e4eec5a1ee961de50d6e
MatteFlorian <matteflorian@live.fr>
2018-06-04 21:21:26 +0200

    First commit

---
+++ tumblrpy.py
@@ -0,0 +1,35 @@
+import pytumblr
+import requests
+from tumblr_keys import *    #this imports the content in our tumblr_keys.py file
+
+# Authenticate via OAuth
+client = pytumblr.TumblrRestClient(
+    consumer_key,
+    consumer_secret,
+    token_key,
+    token_secret
+)
+
+def run():
+    feed = client.posts('good-smile-company', limit=1)
+
+    #print(feed['posts'])
+    announce = []
+
+    for post in feed['posts']:
+        #Get tag
+        tag = post['tags']
+        if ('nendoroid' in tag): #treatment
+            #Get name of the Fig
+            name = ' '.join(tag).split('nendoroid ')[1]
+            #Get link
+            link = post['caption'].split('<a href=\"')[1].split('\">')[0]
+            #Get first picture
+            for photos in post['photos']:
+                original = photos['original_size']
+                url = original['url']
+                break
+
+            #Now we send it to image
+            announce.append([url,name,link])
+    return announce
