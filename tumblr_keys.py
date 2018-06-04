8def5e5578c126f41d2c713878d738edf4bbbecb
Drake62610 <matteflorian@live.fr>
2018-06-04 21:51:56 +0200

    Delete tumblr_keys.py

--- tumblr_keys.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'qvP0mXWHHgGBYwSecGr9xS1YCDXMuU451yoRNJkQABAqI6IxK7'
-consumer_secret = 'wfQ6relNzDZqPh2VMoCllawb5rRlUFP60ak5IqUW0UtMungprk'
-token_key = 'iy2Xw6W86orYYniFuqfjReSIC81ptRFjU82YsJufJ5PRIz79Ob'
-token_secret = 'm77YiHXahRcW3ugbm8rbndrjnoBmUZS0FIYibsaNf1h1vFRIph'
