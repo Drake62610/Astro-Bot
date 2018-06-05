d6a379d948405e8273fad4f4977d0e6fd514e629
MatteFlorian <matteflorian@live.fr>
2018-06-05 20:07:11 +0200

    Globalisation, need fix

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import recastai
 import nyaaRSS
 import tumblrpy
 import requests
+import fgoroll
 from discord.ext import commands
 from datetime import date
 from secretkey import *
@@ -175,6 +176,23 @@ async def currentevent():
 async def idchannel(ctx, arg):
     id = [channel for channel in bot.get_all_channels() if channel.name == arg][0]
     await bot.say("L\' id du channel " + arg + " est : " + id)
+
+@bot.command()
+async def roll(number,mode,isTicket):
+    print(number)
+    print(mode)
+    print(isTicket)
+    gacha = fgoroll.Gacha()
+    gacha.changeMode(mode)
+    result = gacha.simulate(number,isTicket)
+    print(result[0].name)
+    msg=""
+    for pulled in result:
+        print(pulled.name)
+        print(pulled.stars)
+        msg = msg + pulled.name + "   " + pulled.stars + "*\n"
+
+    await bot.say(msg)
 
 @bot.command()
 async def nextevent():
