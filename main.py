65a92fe4f58864625f1aebc1a661bd1f58be727a
MatteFlorian <matteflorian@live.fr>
2018-06-18 20:08:58 +0200

    Minor fix

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
     for extension in startup_extensions:
         try:
             bot.load_extension(extension)
+            print('Extension ' + extension + ' has been loaded.')
         except Exception as e:
             exc = '{}: {}'.format(type(e).__name__, e)
             print('Failed to load extension {}\n{}'.format(extension, exc))
