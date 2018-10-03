6ae71ff2c28ff4b417f68a17b26e845a8292eaca
MatteFlorian <matteflorian@live.fr>
2018-10-03 13:32:12 +0200

    Ajout de la fonction !randomANL et !searchANL

--- anilist/AnimeCommands.py
+++ anilist/AnimeCommands.py
@@ -15,7 +15,8 @@ class AnimeCommands():
         self.logger = logging.getLogger('AstroLog')
 
     @commands.command()
-    async def randomANL(self):
+    async def randomANL(self,*nums):
+        link = ""
          #First Query to obtain number of pages
         query = '''
         {
@@ -50,7 +51,40 @@ class AnimeCommands():
         '''
         variables = {'random' : random}
         response = requests.post(url, json={'query': query, 'variables': variables})
-        await self.bot.say(response.json()['data']['Page']['media'][0]['title']['romaji'])
+        title = response.json()['data']['Page']['media'][0]['title']['romaji']
+        #Last query to get anime information
+        media = searchANL(title)[0] #Get first result
+        print(media)
+        genre = media['genres']
+        strGenre = ''
+        for i in genre:
+            strGenre = strGenre + ' ' + i
+        tag = media['tags']
+        strTag = ''
+        for i in tag:
+            strTag = strTag + ' ' + i['name']
+        link = "https://anilist.co/anime/" + str(media['id'])
+        # if("-a" in nums):
+        #     pass
+        # else:
+        #     if("-l" in nums):
+        #
+        #     if("-i" in nums):
+        #         pass
+        await self.bot.say("Vous avez tiré : " +
+                           "\n**__Titre :__** "     + title +
+                           "\n**__Genre(s) :__** " + strGenre +
+                           "\n**__Tag(s) :__** " + strTag +
+                           "\n" + link
+                          )
+
+    @commands.command()
+    async def searchANL(self,name,type="ANIME"):
+        resp = searchANL(name,type)[0]
+        title = resp['title']['romaji']
+        link = "https://anilist.co/"+ type.lower() + "/" + str(resp['id'])
+        print(resp)
+        await self.bot.say(title + "\n" + link)
 
 def setup(bot):
     bot.add_cog(AnimeCommands(bot))
@@ -65,3 +99,49 @@ def setup(bot):
 #     if not self.arithmeticMean or not self.weightedMean:
 #       return False
 #     return self.arithmeticMean >= threshold and self.weightedMean >= threshold
+
+#Get 3 first anime of a search query in anilist
+#Return tab of anime
+def searchANL(name,type="ANIME"):
+    query = '''
+    query ($id: Int, $page: Int, $perPage: Int, $search: String, $type: MediaType) {
+      Page(page: $page, perPage: $perPage) {
+        pageInfo {
+          total
+          currentPage
+          lastPage
+          hasNextPage
+          perPage
+        }
+        media(id: $id, search: $search, type:$type) {
+          id
+          title {
+            romaji
+          }
+          type
+          episodes
+          coverImage {
+            large
+          }
+          trailer {
+            id
+            site
+          }
+          genres
+          averageScore
+          tags {
+            name
+          }
+        }
+      }
+    }
+    '''
+    variables = {
+        'search': name,
+        "type": type,
+        'page': 1,
+        'perPage': 3
+    }
+    url = 'https://graphql.anilist.co'
+    response = requests.post(url, json={'query': query, 'variables': variables})
+    return response.json()['data']['Page']['media']
