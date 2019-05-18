d55a909b7fd91fed58fa811f92a927680f2a8393
Drake62610 <matteflorian@live.fr>
2019-05-18 08:45:52 -0700

    add keys.py

---
+++ keys.py
@@ -0,0 +1,7 @@
+import os
+
+consumer_key = os.environ['TUMBLR_CONSUMER_KEY']
+consumer_secret = os.environ['TUMBLR_CONSUMER_SECRET']
+token_key = os.environ['TUMBLR_TOKEN_KEY']
+token_secret = os.environ['TUMBLR_TOKEN_SECRET']
+main_key = os.environ['MAIN_KEY']
