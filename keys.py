3c8c8e97b57fe783122d68a245ebffeaf7a58332
Drake62610 <matteflorian@live.fr>
2019-07-20 11:31:12 -0700

    minor fix

--- keys.py
+++ keys.py
@@ -1,11 +1,5 @@
 import os
 
-os.environ['TUMBLR_CONSUMER_KEY'] = "qvP0mXWHHgGBYwSecGr9xS1YCDXMuU451yoRNJkQABAqI6IxK7"
-os.environ['TUMBLR_CONSUMER_SECRET'] = "wfQ6relNzDZqPh2VMoCllawb5rRlUFP60ak5IqUW0UtMungprk"
-os.environ['TUMBLR_TOKEN_KEY'] = "iy2Xw6W86orYYniFuqfjReSIC81ptRFjU82YsJufJ5PRIz79Ob"
-os.environ['TUMBLR_TOKEN_SECRET'] = "m77YiHXahRcW3ugbm8rbndrjnoBmUZS0FIYibsaNf1h1vFRIph"
-os.environ['MAIN_KEY'] = "MzAxMzkzMzY5Mjk5NDE5MTQ4.D2Z43Q.BvsJdB-MQ8SMOhxGrKvkZM6bx5g"
-
 consumer_key = os.environ['TUMBLR_CONSUMER_KEY']
 consumer_secret = os.environ['TUMBLR_CONSUMER_SECRET']
 token_key = os.environ['TUMBLR_TOKEN_KEY']
